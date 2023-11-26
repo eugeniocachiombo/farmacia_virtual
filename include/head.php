1f8a0e2056065123ca1c8005cba63df3b0b5d1df
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-11-26 21:09:12 +0100

    Modelo inicial do design

---
+++ include/head.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/fontawesome/css/all5.css">
+    <link rel="stylesheet" href="../assets/bootstrap-5.0.2-dist/css/bootstrap.css">
+    <script src="../assets/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="../assets/css/style_main.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="container-fluid bg-white text-dark p-2">
+            <div class=" d-flex align-items-center">
+                <div class="col-2 col-md-6 col-lg-6 text-start" >
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <button class=" navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false"
+                            aria-label="Toggle navigation" style="">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </nav>
+                </div>
+
+                <div class="col-10 col-md-6 col-lg-6 text-end" style="color: #36805D;">
+                    <i class="fas fa-user pe-1"></i> <b>User Name</b>
+                </div>
+            </div>
+
+            <div class="">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="container-fluid">
+                        <div class="collapse navbar-collapse" id="navbarText">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Features</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Pricing</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="container " style="min-height: inherit">
+            <div class="row d-flex align-items-center" style="min-height: inherit">
+                <div class="col-12 col-md-4 col-lg-6 text-center text-md-start">
+                    <i class="fas fa-cannabis" style="font-size: 100px;"></i>
+                </div>
+
+                <div class="d-none d-md-block col-md-8 col-lg-6 text-center text-md-end">
+                    <h1><b><i class="fas fa-flask"></i> Farmácia Virtual</b></h1>
+                </div>
+
+                <div class="d-blcok d-md-none col-12 text-center" style="margin-top: -140px">
+                    <h1><b><i class="fas fa-flask"></i> Farmácia Virtual</b></h1>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php include "../include/head.php"; ?>
+
+<main>
+  
+</main>
+
+<footer>
+    rodapé
+</footer>
+
+<?php include "../include/foot.php"; ?>
